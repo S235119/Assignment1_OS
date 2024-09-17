702410070f03d05da746fd4d54351a387c28301f
Rebecca Moss <s225042@dtu.dk>
2024-09-17 14:02:44 +0200

    fungere

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 /* You are not allowed to use <stdio.h> */
 #include "io.h"
 #include <unistd.h>
+#include <stdlib.h>
 
 
 
@@ -12,7 +13,7 @@
  * @return 0 for success, anything else for failure
  *
  *
- * Then it has a place for you to implementation the command 
+ * Then it has a place for you to implementation the command
  * interpreter as  specified in the handout.
  */
 int
@@ -23,62 +24,101 @@ main()
 
 
 
-       /*-----------------------------------------------------------------
-   *TODO:  You need to implement the command line driver here as
-   *       specified in the assignment handout.
-   *
-   * The following pseudo code describes what you need to do
-   *  
-   *  Declare the counter and the collection structure variables
-   *
-   *
-   *  In a loop
-   *    1) Read a command from standard in using read_char function
-   *    2) If the command is not 'a', 'b', 'c': then break the loop
-   *    3) Process the command as specified in the handout
-   *  End loop
-   *
-   *  Print your collection of elements as specified in the handout
-   *    as a comma delimited series of integers
-   *-----------------------------------------------------------------*/
+    /*-----------------------------------------------------------------
+*TODO:  You need to implement the command line driver here as
+*       specified in the assignment handout.
+*
+* The following pseudo code describes what you need to do
+*
+*  Declare the counter and the collection structure variables
+*
+*
+*  In a loop
+*    1) Read a command from standard in using read_char function
+*    2) If the command is not 'a', 'b', 'c': then break the loop
+*    3) Process the command as specified in the handout
+*  End loop
+*
+*  Print your collection of elements as specified in the handout
+*    as a comma delimited series of integers
+*-----------------------------------------------------------------*/
+    typedef struct Collection {
+        int element;
+        struct Collection* next;
+    } Collection;
 
-    int count = 0;
-    int collection[100] = {0};
-    int number = 0;
     char character;
 
-    do{
-        character = read_char();
 
-        if(character == 'a'){
-            collection[number] = count;
-            number++;
+    /*  Collection * createCollection(int element) {
+          character = read_char();
+          Collection* newElement = (Collection*)malloc(sizeof(Collection));
+          newElement=character;
+          if (newElement == NULL) {
+              write_string("Error\n");
+              return NULL;
+          }
+          newElement->element = element;
+          newElement->next = NULL;
+          return newElement;
+      }*/
+
+
+    Collection *head = NULL;
+    Collection *tail = NULL;
+
+
+    int count = 0;
+
+    do{
+        Collection *newElement = (Collection*)malloc(sizeof(Collection));
+        character = read_char();
+        if(character == 'a') {
+            newElement->element = count;
+            newElement->next = NULL;
+
+            if(head == NULL){
+                head = newElement;
+                tail = newElement;
+            } else{
+                tail -> next = newElement;
+                tail = newElement;
+            }
             count++;
         }
         if(character == 'b'){
             count++;
         }
+
         if(character == 'c'){
-            if (number !=0) {
-                number--;
+            if(head != NULL){
+                if(head == tail){
+                    free(head);
+                    head = NULL;
+                    tail = NULL;
+                } else{
+                    Collection *current = head;
+                    while (current->next != tail) {
+                        current = current->next;
+                    }
+                    free(tail);
+                    tail = current;
+                    tail ->next = NULL;
+                }
             }
-            collection[number] = 0;
             count++;
         }
     }
     while (character == 'a' || character == 'b' || character == 'c');
 
-    int spot = 0;
-    for (int y = 0; y < number; y++) {
-        while (collection[spot] != 0 || spot == 0) {
-            write_int(collection[spot]);
-            spot++;
-            if (collection[spot] != 0) {
-                write_char(',');
-            }
+    while (head != NULL) {
+        write_int(head -> element);
+        if (head -> next != NULL) {
+            write_char(',');
         }
+        head = head -> next;
     }
-    write_string(";");
+    write_char(';');
     write_char('\n');
 
     /*
